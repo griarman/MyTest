b078b59e26dbd6a4ae69539fc920ba825ab42f46
Arman <grigoryan.arman@bk.ru>
2018-07-11 03:34:39 +0400

    3.1 changed the database

--- SQL/shops.sql
+++ SQL/shops.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1
--- Время создания: Мар 15 2018 г., 18:03
--- Версия сервера: 10.1.21-MariaDB
--- Версия PHP: 5.6.30
+-- Host: 127.0.0.1
+-- Generation Time: Jul 11, 2018 at 01:30 AM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,14 +19,14 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `shops`
+-- Database: `shops`
 --
-	CREATE DATABASE shops;
-	USE shops;
+  CREATE DATABASE shops;
+  USE shops;
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `admin`
+-- Table structure for table `admin`
 --
 
 CREATE TABLE `admin` (
@@ -33,7 +35,7 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Дамп данных таблицы `admin`
+-- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`login`, `password`) VALUES
@@ -42,7 +44,7 @@ INSERT INTO `admin` (`login`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `categories`
+-- Table structure for table `categories`
 --
 
 CREATE TABLE `categories` (
@@ -51,16 +53,18 @@ CREATE TABLE `categories` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_croatian_ci;
 
 --
--- Дамп данных таблицы `categories`
+-- Dumping data for table `categories`
 --
 
 INSERT INTO `categories` (`id`, `name`) VALUES
-(1, 'Shoes');
+(1, 'Shoes'),
+(2, 'Dress'),
+(3, 'Sweatshirt');
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `images`
+-- Table structure for table `images`
 --
 
 CREATE TABLE `images` (
@@ -69,10 +73,24 @@ CREATE TABLE `images` (
   `image` tinytext CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `prod_id`, `image`) VALUES
+(1, 1, '../images/2/20180329131834673185751.jpg'),
+(2, 1, '../images/2/201803291307111660495891.jpg'),
+(3, 10, '../images/3/201806191430431764309548.jpg'),
+(4, 10, '../images/3/20180619143043964899802.jpg'),
+(5, 10, '../images/3/201806191430431210647174.jpg'),
+(6, 11, '../images/1/20180628141612253409290.jpg'),
+(7, 12, '../images/1/201806291703401611576215.jpg'),
+(8, 13, '../images/2/201806292338061994438055.jpg');
+
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `orders`
+-- Table structure for table `orders`
 --
 
 CREATE TABLE `orders` (
@@ -81,10 +99,18 @@ CREATE TABLE `orders` (
   `order_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_croatian_ci;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `userid`, `order_date`) VALUES
+(1, 1, '2018-07-09'),
+(2, 1, '2018-07-10');
+
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `order_details`
+-- Table structure for table `order_details`
 --
 
 CREATE TABLE `order_details` (
@@ -93,38 +119,75 @@ CREATE TABLE `order_details` (
   `qunatity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_croatian_ci;
 
+--
+-- Dumping data for table `order_details`
+--
+
+INSERT INTO `order_details` (`orderId`, `productId`, `qunatity`) VALUES
+(1, 10, 7),
+(1, 13, 3),
+(2, 11, 6);
+
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `products`
+-- Table structure for table `products`
 --
 
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `name` varchar(20) COLLATE utf8mb4_croatian_ci NOT NULL,
-  `price` int(11) NOT NULL,
+  `price` float NOT NULL,
   `cat_id` int(11) NOT NULL,
   `description` mediumtext COLLATE utf8mb4_croatian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_croatian_ci;
 
 --
--- Дамп данных таблицы `products`
+-- Dumping data for table `products`
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `cat_id`, `description`) VALUES
-(19, 'sdfsdf', 34, 1, 'sdfsdf'),
-(20, 'aaaa', 123, 1, 'asdasd'),
-(21, 'aaaa', 123, 1, 'asdasd'),
-(22, 'gfggfhjgfgf', 500, 1, 'ghjghghghjgh');
+(1, 'Shoes', 99, 1, 'Brown shoes'),
+(2, 'name1', 15, 1, 'description 1'),
+(3, 'name2', 115, 2, 'description 2'),
+(4, 'name3', 1.5, 1, 'description 3'),
+(5, 'name4', 10, 2, 'description 4'),
+(6, 'name5', 25, 2, 'description 5'),
+(7, 'name6', 34.47, 1, 'description 6'),
+(8, 'name7', 96, 2, 'description 7'),
+(9, 'name8', 63, 1, 'description 8'),
+(10, 'Hooded Sweatshirt', 39.95, 3, 'The Last Word With Lawrence O Donell Lightweight Zip Up Hooded Sweatshirt'),
+(11, 'name20', 659, 1, 'dasadasdsdasdasdsa'),
+(12, 'name 7', 120, 1, 'Desc 10'),
+(13, 'gghftyrty', 11, 2, 'Dress');
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `users`
+-- Table structure for table `token`
+--
+
+CREATE TABLE `token` (
+  `userId` int(11) NOT NULL,
+  `token` varchar(32) NOT NULL,
+  `expireDate` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `token`
+--
+
+INSERT INTO `token` (`userId`, `token`, `expireDate`) VALUES
+(1, '85d647b4d4350aab79d0c3809f4d632c', 1531870011);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
-  `id` int(3) NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(20) COLLATE utf8mb4_croatian_ci NOT NULL,
   `login` varchar(30) COLLATE utf8mb4_croatian_ci NOT NULL,
   `password` varchar(64) COLLATE utf8mb4_croatian_ci NOT NULL,
@@ -132,106 +195,132 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_croatian_ci;
 
 --
--- Индексы сохранённых таблиц
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `login`, `password`, `mail`) VALUES
+(1, 'Arman', 'griarman', 'e10adc3949ba59abbe56e057f20f883e', 'grigoryan.arman@bk.ru'),
+(2, 'Arm', 'griarm', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'grigoryan.arman1996@gmail.com');
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Индексы таблицы `categories`
+-- Indexes for table `categories`
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
--- Индексы таблицы `images`
+-- Indexes for table `images`
 --
 ALTER TABLE `images`
   ADD PRIMARY KEY (`id`),
   ADD KEY `prod_id` (`prod_id`);
 
 --
--- Индексы таблицы `orders`
+-- Indexes for table `orders`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
   ADD KEY `userid` (`userid`);
 
 --
--- Индексы таблицы `order_details`
+-- Indexes for table `order_details`
 --
 ALTER TABLE `order_details`
   ADD PRIMARY KEY (`orderId`,`productId`),
   ADD KEY `productId` (`productId`);
 
 --
--- Индексы таблицы `products`
+-- Indexes for table `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
   ADD KEY `cat_id` (`cat_id`);
 
 --
--- Индексы таблицы `users`
+-- Indexes for table `token`
+--
+ALTER TABLE `token`
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT для сохранённых таблиц
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT для таблицы `categories`
+-- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
--- AUTO_INCREMENT для таблицы `images`
+-- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
--- AUTO_INCREMENT для таблицы `orders`
+-- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
--- AUTO_INCREMENT для таблицы `products`
+-- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
 --
--- AUTO_INCREMENT для таблицы `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
--- Ограничения внешнего ключа сохраненных таблиц
+-- Constraints for dumped tables
 --
 
 --
--- Ограничения внешнего ключа таблицы `images`
+-- Constraints for table `images`
 --
 ALTER TABLE `images`
   ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`prod_id`) REFERENCES `products` (`id`);
 
 --
--- Ограничения внешнего ключа таблицы `orders`
+-- Constraints for table `orders`
 --
 ALTER TABLE `orders`
   ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ограничения внешнего ключа таблицы `order_details`
+-- Constraints for table `order_details`
 --
 ALTER TABLE `order_details`
   ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`orderId`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`productId`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ограничения внешнего ключа таблицы `products`
+-- Constraints for table `products`
 --
 ALTER TABLE `products`
   ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `token`
+--
+ALTER TABLE `token`
+  ADD CONSTRAINT `token_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
